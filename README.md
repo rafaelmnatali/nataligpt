6e5c7430e0af86eecde10db61ee6e229ea86e865
Rafael Natali <rafael.natali@gmail.com>
2023-10-05 16:34:32 +0100

    ask general questions

---
+++ README.md
@@ -0,0 +1,41 @@
+# nataligpt
+
+This extension allows you to interact with [ChatGPT](https://chat.openai.com/) from within the Visual Studio.
+
+:warning: The presented solutions are intended for learning purposes. Hence, don't treat as production-ready.
+
+## Features
+
+Ask general questions to query ChatGPT via an input box in the sidebar
+
+## Requirements
+
+This extension was tested using `Visual Studio Code=1.82.0` and `Node.js=9.6.7`
+
+See the [package.json](./package.json) file for details.
+
+## Installation
+
+To use this extension, install it from the VSCode marketplace.
+
+Once the installation is finished, you'll need to insert your ChatGPT session token into the extension settings in VSCode. To accomplish this, follow these steps:
+
+1. Open the "Code" menu.
+2. Select "Preferences" from the dropdown menu.
+3. Choose "Settings" to access the settings panel.
+4. In the search bar, type `nataligpt` to filter the settings list.
+5. Enter your session token in the `API Key` field.
+
+After completing these steps, the extension should be ready to use.
+
+## Obtaining the API Key
+
+To use this extension, you will need to authenticate with a valid API Key from ChatGPT. To get an API Key refer to [OpenAI docs](https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key)
+
+## Contributing
+
+Feel free to contribute by opening issues or pull requests.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](../LICENSE) file for details.  
---
+++ extension.js
@@ -0,0 +1,72 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "nataligpt" is now active!');
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand('nataligpt.chatGPT', async () => {
+          const userInput = await vscode.window.showInputBox({
+            placeHolder: 'Type your message to ChatGPT',
+          });
+      
+          if (userInput) {
+            const response = await sendToChatGPT(userInput);
+            vscode.window.showInformationMessage(`ChatGPT: ${response}`);
+          }
+        })
+      );
+	
+	
+}
+
+async function sendToChatGPT(text) {
+	// const openaiApiKey = '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';
+	// Get the API session token from the extension's configuration
+	const config = vscode.workspace.getConfiguration('nataligpt');
+	const openaiApiKey = config.get('API Key');
+	
+	if (!openaiApiKey) {
+		vscode.window.showErrorMessage('Please set your OpenAI API key in the extension settings.');
+	}
+
+	try {
+		const response = await fetch('https://api.openai.com/v1/chat/completions', {
+			method: 'POST',
+			headers: {
+				  'Content-Type': 'application/json',
+				  'Authorization': `Bearer ${openaiApiKey}`,
+			},
+			body: JSON.stringify({
+				model: "gpt-3.5-turbo",
+				messages: [{"role": "user", "content": text}]
+			})
+		})
+	
+		const data = await response.json()
+    	return data.choices[0].message.content
+
+	  } catch (error) {
+		console.error('Error sending request to ChatGPT:', error);
+		return 'An error occurred while connecting to ChatGPT.';
+	  }
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
